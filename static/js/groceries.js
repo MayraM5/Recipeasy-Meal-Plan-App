769524e0a02272ee92fee7c74cfb64870f5822ab
MayraM5 <mayra.mercado@tuta.io>
2022-12-26 13:34:31 -0500

    Updated grocery list by category

---
+++ static/js/groceries.js
@@ -0,0 +1,12 @@
+// function check_item(input_id) {
+//     let checked_input = document.querySelector("input[id=" + input_id + "]");
+//     let checked_label = document.querySelector("label[name=" + input_id + "]");
+
+//     if (checked_input.checked){
+//         checked_label.style.textDecoration = "line=throught";
+//     }
+//     else {
+//         checked_label.style.textDecoration = "";
+//     }
+// }
+
