9330686163d1c6ff034dcfcecbaa8ad8d7d295a7
MayraM5 <mayra.mercado@tuta.io>
2023-01-06 12:58:14 -0500

    clean code

--- seed_database.py
+++ seed_database.py
@@ -7,22 +7,15 @@ import server
 os.system('dropdb mealplanning')
 os.system('createdb mealplanning')
 
-#from model import db, connect_to_db
-#import server
-
+# Connect app to database
 model.connect_to_db(server.app, "mealplanning")
 model.db.create_all()
 
-# Connect app to database
-#connect_to_db(server.app)
-
-# Updates the database schema
-#db.create_all()
-#import crud
 
 user = crud.create_user(first_name="Mary", last_name="Test", email="mary@test.com", password="12345")
 # user = crud.create_user(email, first_name, last_name, password)
 model.db.session.add(user)
 model.db.session.commit()
 
- 
+ # Updates the database schema
+model.db.create_all()
